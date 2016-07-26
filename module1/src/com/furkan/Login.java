80fdecc750e7e7e7b2f2753f5fba22a54d198a76
spootrick <myoiatr@gmail.com>
2016-07-26 05:02:43 -0400

    test fix for 404

--- module1/src/com/furkan/Login.java
+++ module1/src/com/furkan/Login.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/login")
+@WebServlet(urlPatterns = "/module1/login")
 public class Login extends HttpServlet {
 
 	@Override
